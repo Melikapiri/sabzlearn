cd4af9ebfb9a041bbc5736b1b407f66aac3cae03
Melikapiri <pirimelika9@gmail.com>
2025-07-20 14:45:04 +0330

    first commit

---
+++ pages/api/course/index.js
@@ -0,0 +1,17 @@
+import connectionDb from "@/data/connectionDB"
+import courseModel from '@/models/course'
+
+const userHandler = async (req, res) => {
+    await connectionDb()
+    switch (req.method) {
+        case "GET": {
+
+
+
+
+        }
+
+    }
+}
+
+export default userHandler
---
+++ pages/search/index.js
@@ -0,0 +1,29 @@
+import {useState} from "react";
+import coursesItem from "@/components/modules/coursesItem/CoursesItem";
+import styles from "@/styles/Course.module.css";
+import CoursesItem from "@/components/modules/coursesItem/CoursesItem";
+
+
+const Search = () => {
+
+
+    return (
+        <>
+            {/*<section className={styles.courses}>*/}
+            {/*    <div className={styles.courses_top}>*/}
+            {/*        <h2 className={styles.courses_title}>نتایج جستجو برای : </h2>*/}
+
+            {/*    </div>*/}
+            {/*    <ul className={styles.courses_list}>*/}
+            {/*        <CoursesItem/>*/}
+            {/*    </ul>*/}
+            {/*</section>*/}
+من سرچ هستم
+            
+
+        </>
+    );
+};
+
+
+export default Search;
